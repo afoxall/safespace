855b8d22886eabfad59543b7b6290bf5d1207b45
Alisha Foxall <13amf7@queensu.ca>
2016-12-03 14:42:07 -0500

    it pops up a warning screen, working on making that screen better

--- scanner.js
+++ scanner.js
@@ -1,9 +1,29 @@
 
 //var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
 
-var triggers = ["Hack"]
+var triggers = {"Hack": false};
 
 walk(document.body, triggers);
+var numfound = 0;
+
+//check if any of the things in the array are true
+for(key in triggers){
+    if(triggers[key]){
+        numfound += 1;
+    }
+}
+
+
+if(numfound > 0){
+    alert("We found " + numfound + " of your triggers on this site.");
+
+    chrome.runtime.sendMessage({"type": "warning", "triggers": triggers }, function(response) {
+           console.log(response.farewell);
+    });
+
+}
+
+
 //setTimeout(function () {
 //    walk(document.body, triggers);
 //    }, 1000);
@@ -47,18 +67,19 @@ function handleText(textNode, triggers)
 {
 	var val = textNode.nodeValue;
 
-	if (triggers.some(function(t) { return val.includes(t)})) {
-            //make a popup to say that we found a match
-            alert("We found one of your triggers on this page");
-        }
-    
-}
-/*    
-    while(walk.nextNode()) {
-        var node = walk.currentNode();
-        if (triggers.some(function(v) { return node.indexOf(v) >= 0; })) {
-            //make a popup to say that we found a match
-            alert("We found one of your triggers on this page");
+ 
+    for(key in triggers){
+        if(val.includes(key)){
+            triggers[key] = true;
         }
     }
-}*/
+    /*       
+    }        
+	if (triggers.keys.some(function(t) { return val.includes(t)})) {
+            //make a popup to say that we found a match
+            triggers[t] = true;
+        }
+ */   
+}
+
+

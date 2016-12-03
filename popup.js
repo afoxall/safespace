7aa4127e016b92c42b1c4f074f9ce502bb39d25a
Jackie Bettencourt <jackie@C-3PO.local>
2016-12-03 16:20:21 -0500

    for mohamad

--- popup.js
+++ popup.js
@@ -1,22 +1,43 @@
-/*function displayInputs() {
-   
-}*/
+function displayInputs() {
+ var triggerList = ["help", "me", "i'm", "poor"];
+ for (i in triggerList) {
+     addTriggers(triggerList[i]);
+ }
+}
 
-function addInput(text) {
+function addTriggers(text) {
+  var wrapper, input, remove;
+  wrapper = document.getElementById('triggers');
+  input = document.createElement("input");
+  input.type = "text";
+  input.name = text;
+  input.value = text;
+  remove = document.createElement("button");
+  remove.type = "remove_btn";
+  remove.id = "remove_btn";
+  remove.value = "X";
+  wrapper.insertBefore(document.createElement("br"), wrapper.firstChild);
+  wrapper.insertBefore(remove, wrapper.firstChild);
+  wrapper.insertBefore(input, wrapper.firstChild);
+}
+
+function addInput() {
   var wrapper = document.getElementById('triggers');
   var input = document.createElement("input");
   input.type = "text";
-  input.name = text;
+  input.name = "";
   wrapper.appendChild(input);
   wrapper.appendChild(document.createElement("br"));
 }
 
 function saveOptions() {
-  alert("Are you working?");
+  confirm("Are you working?");
 }
 
 function contentLoaded() {
-  var save_btn, add_btn, aboutDiv;
+  displayInputs();
+
+  var save_btn, add_btn;
   save_btn = document.getElementById('save_btn');
   add_btn = document.getElementById('add_btn');
 

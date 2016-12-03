899c63432655f22bb6577a6efb6de183971f3e08
Jackie Bettencourt <jackie@C-3PO.local>
2016-12-03 15:00:04 -0500

    Implementng listening for the submit and add button

--- popup.js
+++ popup.js
@@ -1,12 +1,24 @@
+function addInput() {
+  var wrapper = document.getElementById('triggers');
+  var input = document.createElement("input");
+  input.type = "text";
+  input.name = "test";
+  wrapper.appendChild(input);
+  wrapper.appendChild(document.createElement("br"));
+}
+
+function saveOptions() {
+  alert("Are you working?");
+}
+
 function contentLoaded() {
-  var save_btn;
+  var save_btn, add_btn;
   save_btn = document.getElementById('save_btn');
+  add_btn = document.getElementById('add_btn');
 
 
-  save_btn.addEventListener('click', function() {
-      if (confirm('Does this work?')) {
-          saveOptions();
-      }
+  save_btn.addEventListener('click', saveOptions);
+  add_btn.addEventListener('click', addInput);
 }
 
 document.addEventListener('DOMContentLoaded', contentLoaded);
